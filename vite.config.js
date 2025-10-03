b610413bdcccd17bf176cec376d8b5f753106356
malik <ma037646@gmail.com>
2025-10-03 17:58:06 +0300

    Merge branch 'feat-restrict-banned-users' of https://github.com/Malik-272/mentorship-platform-frontend into feat-restrict-banned-users

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import eslint from "vite-plugin-eslint";
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/",
   plugins: [react(), eslint()],
   // server: {
   //   proxy: {
