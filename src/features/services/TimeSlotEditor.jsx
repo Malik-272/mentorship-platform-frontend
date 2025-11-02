20748bc3e22ace327b42abc34f4a4911d6c2d420
Ahmad Albarasy <ahmadbarasy@gmail.com>
2025-11-02 23:25:36 +0200

    made timeslot options increment by 5

--- src/features/services/TimeSlotEditor.jsx
+++ src/features/services/TimeSlotEditor.jsx
@@ -5,7 +5,7 @@ import { Check, X, Edit2, Loader2, Moon } from "lucide-react";
 
 const TIME_OPTIONS = [];
 for (let hour = 0; hour < 24; hour++) {
-  for (let minute = 0; minute < 60; minute += 15) {
+  for (let minute = 0; minute < 60; minute += 5) {
     const timeString = `${hour.toString().padStart(2, "0")}:${minute
       .toString()
       .padStart(2, "0")}`;
